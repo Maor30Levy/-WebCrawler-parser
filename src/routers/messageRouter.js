ffd28a003d41b9c453a0a616367ab447407e6ea4
unknown <maor30levy@gmail.com>
2021-03-16 17:31:10 +0200

    4.1

--- src/routers/messageRouter.js
+++ src/routers/messageRouter.js
@@ -4,21 +4,11 @@ const router = new express.Router();
 
 router.post('/', async (req, res) => {
     try {
-        const url = req.body.url;
-        const result = await parseURL(url);
+        const result = await parseURL(req.body.url);
         res.send(result);
     } catch (err) {
         res.status(404).send(err.message);
     }
 });
-
-router.get('/', async (req, res) => {
-    try {
-        res.status(404).send('Page not found');
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-});
-
 
 module.exports = router;
